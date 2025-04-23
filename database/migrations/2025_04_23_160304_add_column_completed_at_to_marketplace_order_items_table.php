7c8607572c09deae5b8ff607ea59869ba991de11
skuze <skuzevanov@gmail.com>
2025-04-23 16:27:09 +0500

    Созданы миграции для добавления колонки "дата выполнения" в таблицах Заказы и Товары.

---
+++ database/migrations/2025_04_23_160304_add_column_completed_at_to_marketplace_order_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('marketplace_order_items', function (Blueprint $table) {
+            $table->timestamp('completed_at')
+                ->nullable()
+                ->default(null)
+                ->after('seamstress_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('marketplace_order_items', function (Blueprint $table) {
+            $table->dropColumn('completed_at');
+        });
+    }
+};
