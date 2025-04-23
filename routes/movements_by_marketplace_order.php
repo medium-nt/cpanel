08508bd0e50fbac85c19268eade4682681d90af4
skuze <skuzevanov@gmail.com>
2025-04-23 18:38:10 +0500

    Поправил баг с неверной маршрутизацией.

--- routes/movements_by_marketplace_order.php
+++ routes/movements_by_marketplace_order.php
@@ -6,5 +6,5 @@ use App\Models\Order;
 Route::prefix('/movements_by_marketplace_order')->group(function () {
     Route::get('', [App\Http\Controllers\MovementMaterialByMarketplaceOrderController::class, 'index'])
         ->can('viewAny', MovementMaterial::class)
-        ->name('movements_to_workshop.index');
+        ->name('movements_by_marketplace_order.index');
 });
