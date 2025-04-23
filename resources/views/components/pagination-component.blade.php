ed8590b8769e32ed7ae6e000217180a409612bec
skuze <skuzevanov@gmail.com>
2025-04-23 12:08:13 +0500

    Поправил компонент пагинации, чтобы он входил на узкие экраны.

--- resources/views/components/pagination-component.blade.php
+++ resources/views/components/pagination-component.blade.php
@@ -1,5 +1,13 @@
 <div style="text-align: center; font-size: 18px;">
     <div class="btn-group" role="group">
-        {{ $collection->links() }}
+        {{ $collection->onEachSide(1)->links() }}
     </div>
 </div>
+
+<style>
+    @media (max-width: 600px) {
+        .page-link {
+            padding: .5rem .5rem;
+        }
+    }
+</style>
