e2149d61b87b8f5d310d0c64cd73d8d5f87d4640
skuze <skuzevanov@gmail.com>
2025-05-07 12:21:31 +0500

    Поправил маршрут "check_skuz"

--- routes/marketplace_api.php
+++ routes/marketplace_api.php
@@ -3,7 +3,7 @@
 use App\Models\User;
 
 Route::prefix('/marketplace_api')->group(function () {
-    Route::get('', [App\Http\Controllers\MarketplaceApiController::class, 'checkSkuz'])
+    Route::get('check_skuz', [App\Http\Controllers\MarketplaceApiController::class, 'checkSkuz'])
 //        ->can('viewAny', User::class)
         ->name('marketplace_api.checkSkuz');
 });
