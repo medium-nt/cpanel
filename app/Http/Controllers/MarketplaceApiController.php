2b7b0232bc929ed2534d5d433d5c05ffb4555cfc
skuze <skuzevanov@gmail.com>
2025-05-08 08:47:57 +0500

    Добавил скрипт для проверки дубликатов skuz в системе.

--- app/Http/Controllers/MarketplaceApiController.php
+++ app/Http/Controllers/MarketplaceApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Sku;
 use App\Services\MarketplaceApiService;
 
 class MarketplaceApiController extends Controller
@@ -15,6 +16,19 @@ class MarketplaceApiController extends Controller
         return view('marketplace_api.check_skuz', [
             'title' => 'Материалы не найденные в ERP',
             'skuz' => $notFoundSkus,
+        ]);
+    }
+
+    public function checkDuplicateSkuz()
+    {
+        $duplicates = Sku::selectRaw('sku, count(*) as occurrences')
+            ->groupBy('sku')
+            ->havingRaw('COUNT(*) > 1')
+            ->get();
+
+        return view('marketplace_api.check_duplicate_skuz', [
+            'title' => 'Дубли SKU в ERP',
+            'duplicates' => $duplicates,
         ]);
     }
 
--- routes/marketplace_api.php
+++ routes/marketplace_api.php
@@ -6,4 +6,9 @@ Route::prefix('/marketplace_api')->group(function () {
     Route::get('check_skuz', [App\Http\Controllers\MarketplaceApiController::class, 'checkSkuz'])
 //        ->can('viewAny', User::class)
         ->name('marketplace_api.checkSkuz');
+
+    Route::get('check_duplicate_skuz', [App\Http\Controllers\MarketplaceApiController::class, 'checkDuplicateSkuz'])
+//        ->can('viewAny', User::class)
+        ->name('marketplace_api.checkDuplicateSkuz');
+
 });
