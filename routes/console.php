8f3fe4ced8f41ba1b77ee9861bf7c189d607396c
skuze <skuzevanov@gmail.com>
2025-05-10 11:19:49 +0500

    Добавил задание для крона.

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Services\MarketplaceApiService;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+    echo ' Starting schedule';
+    Log::channel('marketplace_api')->info('Тест крона.');
+    MarketplaceApiService::uploadingNewProducts();
+})->everyFiveMinutes();
