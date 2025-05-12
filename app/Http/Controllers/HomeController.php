e6ee3f2668ce28fb4a46d2128df6015f04892ee3
skuze <skuzevanov@gmail.com>
2025-05-12 11:48:08 +0500

    Виджеты на главной сделал ссылками на соответствующие страницы. В Рабочем календаре сделал выбор сотрудника для просмотра его расписания.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Services\MarketplaceOrderItemService;
 use App\Services\MovementMaterialToWorkshopService;
 use App\Services\ScheduleService;
+use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
@@ -13,16 +15,20 @@ class HomeController extends Controller
         $this->middleware('auth');
     }
 
-    public function index()
+    public function index(Request $request)
     {
+        $employeeId = $request->input('employee_id') ?? auth()->id();
+
         return view('home', [
             'title' => 'Дашборд',
-            'events' => ScheduleService::getScheduleByUserId(auth()->id()),
+            'events' => ScheduleService::getScheduleByUserId($employeeId),
             'newMarketplaceOrderItem' => MarketplaceOrderItemService::new(),
             'marketplaceOrderItemInWork' => MarketplaceOrderItemService::toWork(),
             'urgentMarketplaceOrderItem' => MarketplaceOrderItemService::urgent(),
             'notShippedMovements' => MovementMaterialToWorkshopService::getCountNotShippedMovements(),
             'notReceivedMovements' => MovementMaterialToWorkshopService::getCountNotReceivedMovements(),
+            'employees' => User::query()->get(),
+            'currentUserId' => auth()->id()
         ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,63 +13,88 @@
         <div class="card-body">
             <div class="row">
                 <div class="col-md-2">
-                    <div class="info-box">
-                        <span class="info-box-icon bg-success"><i class="fas fa-cart-plus"></i></span>
-                        <div class="info-box-content">
-                            <span class="info-box-text">Новые задания на пошив</span>
-                            <span class="info-box-number">{{ $newMarketplaceOrderItem }}</span>
+                    <a href="{{ route('marketplace_order_items.index', ['status' => 'new']) }}" class="link-black">
+                        <div class="info-box">
+                            <span class="info-box-icon bg-success"><i class="fas fa-cart-plus"></i></span>
+                            <div class="info-box-content">
+                                <span class="info-box-text">Новые задания на пошив</span>
+                                <span class="info-box-number">{{ $newMarketplaceOrderItem }}</span>
+                            </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
 
                 <div class="col-md-2">
-                    <div class="info-box">
-                        <span class="info-box-icon bg-warning"><i class="fas fa-tags"></i></span>
-                        <div class="info-box-content">
-                            <span class="info-box-text">Товары в пошиве</span>
-                            <span class="info-box-number">{{ $marketplaceOrderItemInWork }}</span>
+                    <a href="{{ route('marketplace_order_items.index') }}" class="link-black">
+                        <div class="info-box">
+                            <span class="info-box-icon bg-warning"><i class="fas fa-tags"></i></span>
+                            <div class="info-box-content">
+                                <span class="info-box-text">Товары в пошиве</span>
+                                <span class="info-box-number">{{ $marketplaceOrderItemInWork }}</span>
+                            </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
 
                 <div class="col-md-2">
-                    <div class="info-box">
-                        <span class="info-box-icon bg-danger"><i class="fas fa-bolt"></i></span>
-                        <div class="info-box-content">
-                            <span class="info-box-text">Срочные заказы (FBS)</span>
-                            <span class="info-box-number">{{ $urgentMarketplaceOrderItem }}</span>
+                    <a href="{{ route('marketplace_order_items.index', ['status' => 'new']) }}" class="link-black">
+                        <div class="info-box">
+                            <span class="info-box-icon bg-danger"><i class="fas fa-bolt"></i></span>
+                            <div class="info-box-content">
+                                <span class="info-box-text">Срочные заказы (FBS)</span>
+                                <span class="info-box-number">{{ $urgentMarketplaceOrderItem }}</span>
+                            </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
 
                 <div class="col-md-3">
-                    <div class="info-box">
-                        <span class="info-box-icon bg-secondary"><i class="fas fa-dolly"></i></span>
-                        <div class="info-box-content">
-                            <span class="info-box-text">Не отгруженные поставки в цех</span>
-                            <span class="info-box-number">{{ $notShippedMovements }}</span>
+                    <a href="{{ route('movements_to_workshop.index') }}" class="link-black">
+                        <div class="info-box">
+                            <span class="info-box-icon bg-secondary"><i class="fas fa-dolly"></i></span>
+                            <div class="info-box-content">
+                                <span class="info-box-text">Не отгруженные поставки в цех</span>
+                                <span class="info-box-number">{{ $notShippedMovements }}</span>
+                            </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
 
                 <div class="col-md-3">
-                    <div class="info-box">
-                        <span class="info-box-icon bg-secondary"><i class="fas fa-boxes"></i></span>
-                        <div class="info-box-content">
-                            <span class="info-box-text">Не принятые поставки в цехе</span>
-                            <span class="info-box-number">{{ $notReceivedMovements }}</span>
+                    <a href="{{ route('movements_to_workshop.index') }}" class="link-black">
+                        <div class="info-box">
+                            <span class="info-box-icon bg-secondary"><i class="fas fa-boxes"></i></span>
+                            <div class="info-box-content">
+                                <span class="info-box-text">Не принятые поставки в цехе</span>
+                                <span class="info-box-number">{{ $notReceivedMovements }}</span>
+                            </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
             </div>
         </div>
     </div>
 
     <div class="card">
-        <div class="card-header">
-            <h3 class="card-title">Рабочий календарь</h3>
+        <div class="card-header row">
+            <h4 class=" my-1 mr-2">Рабочий календарь</h4>
+
+            <div class="col-md-3">
+                <select name="employee_id"
+                        id="employee_id"
+                        class="form-control"
+                        onchange="updatePageWithQueryParam(this)"
+                        required>
+                    @foreach($employees as $employee)
+                        <option value="{{ $employee->id }}"
+                                @if(request('employee_id', $currentUserId) == $employee->id) selected @endif
+                        >{{ $employee->name }}</option>
+                    @endforeach
+                </select>
+            </div>
         </div>
         <div class="card-body">
+
             <div id="calendar" data-events="{{ json_encode($events) }}"></div>
         </div>
     </div>
@@ -79,8 +104,11 @@
 @push('js')
     <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.17/index.global.min.js'></script>
     <script src="{{ asset('js/fullcalendar.js') }}"></script>
+    <script src="{{ asset('js/PageQueryParam.js') }}"></script>
 @endpush
 
 @push('css')
     <link href="{{ asset('css/fullcalendar.css') }}" rel="stylesheet"/>
+    <link href="{{ asset('css/link_black.css') }}" rel="stylesheet"/>
+
 @endpush
