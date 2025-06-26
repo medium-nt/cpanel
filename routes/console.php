05db981e83bc0e7ae606d557beba316123ad201b
skuze <skuzevanov@gmail.com>
2025-06-26 15:26:55 +0500

    Разрешил запуск скрипта получения новых заказов с маркетплейса через cron.

--- routes/console.php
+++ routes/console.php
@@ -3,12 +3,11 @@
 use App\Services\MarketplaceApiService;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Log;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
 Schedule::call(function () {
-//    MarketplaceApiService::uploadingNewProducts();
+    MarketplaceApiService::uploadingNewProducts();
 })->everyFiveMinutes();
