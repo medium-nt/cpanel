cae7d5f06ec9af38a8c8176670896b7ebea8d023
skuze <skuzevanov@gmail.com>
2026-01-14 07:15:31 +0500

    Изменение deploy.sh.

--- deploy.sh
+++ deploy.sh
@@ -11,45 +11,40 @@ CYAN="\e[36m"
 RESET="\e[0m"
 
 block() {
-    echo -e "${MAGENTA}\n===== $1 =====${RESET}"
+    echo -e "${CYAN}\n===== $1 =====${RESET}"
 }
 
 echo
-echo -e "${CYAN}=== НАЧАЛО ДЕПЛОЯ ===${RESET}"
+echo -e "${GREEN}=========== НАЧАЛО ДЕПЛОЯ ===========${RESET}"
 echo -e "${BLUE}Текущая директория:${RESET} $(pwd)"
 echo -e "${BLUE}Время:${RESET} $(date)"
-block "==================================="
 
-echo
 block "ОБНОВЛЕНИЕ КОДА (git pull)"
 git pull origin main
-block "==================================="
+echo -e "${CYAN}===================================${RESET}"
 
-echo
 block "УСТАНОВКА ЗАВИСИМОСТЕЙ (composer)"
 /usr/local/bin/php8.4 composer.phar install --no-interaction --prefer-dist --optimize-autoloader --no-dev
-block "==================================="
+echo -e "${CYAN}===================================${RESET}"
 
-echo
 block "ОЧИСТКА КЕШЕЙ LARAVEL"
 php8.4 artisan cache:clear
 php8.4 artisan config:clear
 php8.4 artisan route:clear
 php8.4 artisan view:clear
-block "==================================="
+echo -e "${CYAN}===================================${RESET}"
 
-echo
 block "ПЕРЕСБОРКА КЕШЕЙ LARAVEL"
 php8.4 artisan config:cache
 php8.4 artisan route:cache
 php8.4 artisan view:cache
-block "==================================="
+echo -e "${CYAN}===================================${RESET}"
 
-echo
 block "ОПТИМИЗАЦИЯ"
 php8.4 artisan optimize
-block "==================================="
+echo -e "${CYAN}===================================${RESET}"
 
-echo
-echo -e "${GREEN}\n=== ДЕПЛОЙ ЗАВЕРШЁН ===${RESET}"
+echo -e "${GREEN}\n====== ДЕПЛОЙ ЗАВЕРШЁН ======${RESET}"
 echo -e "${BLUE}Время:${RESET} $(date)"
+echo -e "${GREEN}=============================${RESET}"
+echo
