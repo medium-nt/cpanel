1ec80aa0a011f3bf5c8a43d0bc760eb3a269097b
skuze <skuzevanov@gmail.com>
2026-01-20 08:34:32 +0500

    Тестим вывод http фрейма в https сайте.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->append([
+            \Illuminate\Http\Middleware\TrustProxies::class,
+        ]);
+
         $middleware->alias([
             'require_open_shift' => RequireOpenShift::class,
         ]);
