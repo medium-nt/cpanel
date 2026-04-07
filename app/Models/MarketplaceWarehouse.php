ef340a77fe6020b78d815caf5d6d068df501a711
skuze <skuzevanov@gmail.com>
2026-04-07 12:26:41 +0500

    Создал в БД таблицу для хранения складов/кластеров маркетплейсов.

---
+++ app/Models/MarketplaceWarehouse.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MarketplaceWarehouse extends Model
+{
+    protected $table = 'marketplace_warehouses';
+
+    protected $fillable = [
+        'name',
+        'marketplace_id',
+        'cluster',
+    ];
+}
