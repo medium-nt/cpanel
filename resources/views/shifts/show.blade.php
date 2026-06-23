ba1f4ae2113d95d053a106538cde40b13e82e385
skuze <skuzevanov@gmail.com>
2026-06-23 12:07:46 +0500

    Убрал лишний пробел.

--- resources/views/shifts/show.blade.php
+++ resources/views/shifts/show.blade.php
@@ -87,8 +87,9 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">
-                    <h3 class="card-title">Сотрудники ({{ $employees->count() }}
-                        )</h3>
+                    <h3 class="card-title">
+                        Сотрудники ({{ $employees->count() }})
+                    </h3>
                 </div>
                 <div class="card-body">
                     {{-- Форма добавления --}}
